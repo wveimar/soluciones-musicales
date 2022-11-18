c9eefe772c1932dcc2466a36eab81a31c885078b
wveimar <wveimar.mamian@gmail.com>
2022-11-18 09:43:32 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { setContext } from "@apollo/client/link/context";
+
+const ACCESS_TOKEN = "KKp7X4O3zUWO-Ajm-PCzcyLRB1YiWnNt_5nyc4bmR9I";
+const SPACE_ID = "etnyvhabihji";
+
+const httpLink = createHttpLink({
+  uri: `https://graphql.contentful.com/content/v1/spaces/${SPACE_ID}/environments/master`,
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: ACCESS_TOKEN ? `Bearer ${ACCESS_TOKEN}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
